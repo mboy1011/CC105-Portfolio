5b91e1acbb46c20cdd1088f90c3ea92b5a250b82
mboy1011 <lyjieme@gmail.com>
2020-11-04 14:48:29 +0800

    Added ExpressJS

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-var http = require('http');
-var server = http.createServer(function (req, res) {
- res.writeHead(200, {'Content-Type': 'text/plain'});
- res.end('<h1>Hello, Node.js!</h1>');
+var express = require('express'), path = require('path'),
+ app = express();
+ app.use(express.static(__dirname));
+// app.get(['/', '/index.html'], function (req, res) {
+//  res.writeHead(200, {'Content-Type': 'text/html'});
+//  res.end('<h1>Header</h1><p>Hello, Node.js!</p>');
+// });
+app.get(['/', '/index.html'], function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});   
+app.get('*', function (req, res) {
+ res.writeHead(404, {'Content-Type': 'text/html'});
+ res.end('<h1>404 - Wala na siya nag-paramdam!</h1><p>Ghosting?</p>');
 });
-server.listen(80, '127.0.0.1');
+var server = app.listen(80, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:80/');
